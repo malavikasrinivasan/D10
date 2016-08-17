8210768fe54d37bd4aaeb539021fdea98437d6ff
Malavika Srinivasan <malavikasrinivasan@Malavikas-MacBook-Pro.local>
2016-08-17 11:28:31 -0700

    HW10 w/o bonus

--- numbered_lines.py
+++ numbered_lines.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
+def numbered_lines(filename):
+    with open(filename, "r") as f:
+        file_lines = f.readlines()
+    line_number = 1
+    with open("numbered_lines.txt", "w") as f:
+        for line in file_lines:
+            f.write(str(line_number)+" "+line+"\n")
+            line_number+=1
+
 
 
 def main():
-    numbered_lines()
+    numbered_lines("small.txt")
 
 if __name__ == "__main__":
     main()
